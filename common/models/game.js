526d5b24b308394f47affb18d38d35f2aa3df970
Collin Barlage <cjb356@drexel.edu>
2018-02-25 21:29:46 -0500

    Implement generating random game code

--- common/models/game.js
+++ common/models/game.js
@@ -1,5 +1,21 @@
+var Guid = require('guid');
 'use strict';
 
 module.exports = function(Game) {
 
+	//post game hook
+    Game.observe('before save', function(ctx, next, game) {
+    	//set date modified
+        var currentDate = new Date();
+        if (ctx.isNewInstance == true) {
+            ctx.instance.dateCreated = currentDate;
+        }
+
+        //set key
+        var guid = Guid.create();
+        ctx.instance.key = guid;
+
+    	console.log('Making new Game with key', ctx.instance.key);
+        next();
+    });
 };
